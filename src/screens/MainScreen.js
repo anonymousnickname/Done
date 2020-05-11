5ac341dd055e3d580ceccb08790061baad1c3218
Oleksandr <oleksandrkravets1995@gmail.com>
2020-05-11 17:06:47 +0300

    Added img if it's no any item

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,18 +1,30 @@
 import React from 'react'
-import { StyleSheet, FlatList, View } from 'react-native'
+import { StyleSheet, FlatList, Image, View } from 'react-native'
 
 import { AddForm } from '../components/AddForm'
 import { TodoItem } from '../components/TodoItem' 
 
 export const MainScreen = ({removeTodo, addTodo, doings, showTodo}) => {
-    return (
-        <View>
-        <AddForm onSubmit={addTodo}/>
+    let content = (
         <FlatList
             style={styles.list}
             data={doings}
             keyExtractor={item => item.id}
             renderItem={({item}) => (<TodoItem data={item} onRemove={removeTodo} showTodo={showTodo}/>)}/>
+    )
+
+    if (doings.length === 0) {
+        content = (
+            <View style={styles.imgWrap}>
+                <Image style={styles.image} resizeMode="contain"  source={require('../../assets/noItems.png.png')} />
+            </View>
+        )
+    }
+
+    return (
+        <View>
+        <AddForm onSubmit={addTodo}/>
+            {content}
         </View>
     )
 }
@@ -20,5 +32,15 @@ export const MainScreen = ({removeTodo, addTodo, doings, showTodo}) => {
 const styles = StyleSheet.create({
     list: {
       marginVertical: 15
+    },
+    imgWrap: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        height: 480
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 })
